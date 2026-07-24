e72a9035c8ab02489e5e0e1b5d18c5e19b5db837
manurocky143a-del <manurocky143a-del@users.noreply.github.com>
2026-07-24 18:08:07 +0530

    Fix cloudstream dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ subprojects {
         val implementation by configurations
 
         // Stubs for all Cloudstream classes
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+        cloudstream("com.lagradost:cloudstream3:latest.release")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
